b098f0ae7e653b701a75a65785a8eee0093074a7
Michael Mahemoff <michael@mahemoff.com>
2021-04-01 23:52:00 +1100

    use random edge worker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState('');
+
+  React.useEffect(() => {
+    async function fetchRand() {
+      console.log('rand!');
+      const randomEdgeURL = "https://autumn-hall-70d7.skyflare.workers.dev/?max=500";
+      const randPayload = await fetch(randomEdgeURL)
+      const rand = await randPayload.text();
+      console.log('RAND!', rand);
+      setData(rand);
+    }
+    fetchRand();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,13 +25,16 @@ function App() {
         <p>
           Welcome to <strong>Flares</strong>!!!
         </p>
+        <p>
+          Random: { data }
+        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://example.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I love flares!
         </a>
       </header>
     </div>
